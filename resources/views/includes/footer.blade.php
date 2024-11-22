2c81b0bcabad49a195bf45093dcaa94980d9984b
vahnmarty <vahnmarty@gmail.com>
2024-11-22 16:55:59 +0800

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,24 +1,19 @@
-<section class='bg-white py-20 border-2'>
-    <div>
-        <div class='flex flex-col justify-center items-center'>
-            <a href="https://www.facebook.com/profile.php?id=61564303471877" class='text-black font-semibold text-2xl'>Contact Us</a>
-        </div>
-        <div class='mt-20'>
-            <div class='text-center text-[#868686]'>
-                <p>
-                     Developed By:
-                </p>
-                <p>
-                    Name
-                </p>
-            </div>
-
-            <div class='mt-10 flex flex-col justify-center items-center'>
-                <p class='text-[#868686] text-center'>Powered By:</p>
-                <a href="https://www.facebook.com/ibotikaph/">
-                    <img src="/images/logo/ibotika.webp" alt="ibotika logo" class='h-[90px]'>
-                </a>
-            </div>
-        </div>
+<footer class="bg-gray-800">
+  <div class="px-6 py-12 mx-auto max-w-7xl md:flex md:items-center md:justify-between lg:px-8">
+    <div class="flex justify-center gap-x-6 md:order-2">
+      <a href="https://www.facebook.com/profile.php?id=61564303471877" target="_blank" class="text-gray-400 hover:text-gray-300">
+        <span class="sr-only">Facebook</span>
+        <svg class="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+          <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
+        </svg>
+      </a>
     </div>
-</section>
+    <p class="mt-8 text-center text-gray-400 text-sm/6 md:order-1 md:mt-0">&copy; {{  date('Y') }} PDA MINCON 2025. All rights reserved.</p>
+  </div>
+</footer>
+<div class="bg-gray-900">
+    <div class="justify-center px-6 py-3 mx-auto text-xs text-gray-500 max-w-7xl md:flex md:items-center lg:px-8">
+        <p>Developed by: <span class="text-gray-400">Prynz Palasan</span> & <span  class="text-gray-400">Irish Hambre</span> <span class="mx-8">|</span> Powered by <a href="https://ibotika.com/" target="_blank" class="text-gray-400 underline">@ibotikaph</a></p>
+    </div>
+</div>
+
